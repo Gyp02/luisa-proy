3ea202d07c7a7943ff09b72de6b623c0cdbae3c9
Gypxt <112029218+Gyp02@users.noreply.github.com>
2023-08-28 17:02:05 -0500

    ajuste de routs y parte del inicio del coolever

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,9 +25,11 @@ import { ContentPagesComponent } from './component/content-pages/content-pages.c
   ],
   imports: [
     BrowserModule,
+    FontAwesomeModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
